271b68a48148e6e80d7344da97bdb8b5ddfdfd56
OOKucherenko <110464838+OOKucherenko@users.noreply.github.com>
2023-02-03 22:03:50 +0000

    Deploying to gh-pages from @ OOKucherenko/goit-js-hw-10@46e5e2a9876b24399da6b7fc3f7b83bed1d2bc73 🚀

--- index.14bf48a2.js
+++
@@ -1,2 +0,0 @@
-!function(){let e="";document.querySelector("input#search-box").addEventListener("input",(t=>{e=t.currentTarget.value,fetch(`https://restcountries.com/v2/name/${e}?fields=name,capital,population,flags,languages`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()}))}))}();
-//# sourceMappingURL=index.14bf48a2.js.map
--- index.d186e5c3.js
+++
@@ -1,2 +0,0 @@
-let e="";document.querySelector("input#search-box").addEventListener("input",(t=>{e=t.currentTarget.value,fetch(`https://restcountries.com/v2/name/${e}?fields=name,capital,population,flags,languages`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()}))}));
-//# sourceMappingURL=index.d186e5c3.js.map
