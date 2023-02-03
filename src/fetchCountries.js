46e5e2a9876b24399da6b7fc3f7b83bed1d2bc73
OOKucherenko <okld52dec@gmail.com>
2023-02-04 00:02:39 +0200

    task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 // https://restcountries.com/v2/all?fields=name,capital,currencies
 
 const fetchCountries = name => {
-  fetch(
+  return fetch(
     `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
     if (!response.ok) {
@@ -10,19 +10,5 @@ const fetchCountries = name => {
     return response.json();
   });
 };
-
-//render
-function renderUserList(users) {
-  const markup = users
-    .map(user => {
-      return `<li>
-          <p><b>Name</b>: ${user.name}</p>
-          <p><b>Email</b>: ${user.email}</p>
-          <p><b>Company</b>: ${user.company.name}</p>
-        </li>`;
-    })
-    .join('');
-  userList.innerHTML = markup;
-}
 
 export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import './css/styles.css';
 import fetchCountries from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+let debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 let name = '';
+const countryList = document.querySelector('.country-list');
 const input = document.querySelector('input#search-box');
-input.addEventListener('input', e => {
-  name = e.currentTarget.value;
-  fetchCountries(name);
-});
+
+const changeInput = e => {
+  name = e.target.value.trim();
+  fetchCountries(name)
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (data.length > 1 && data.length <= 10) {
+        renderCountryList(data);
+        console.log('2-10', data);
+      }
+      if ((data.length = 1)) {
+        renderCountryList(data);
+        console.log('1', data);
+      }
+    })
+    .catch(Notify.failure('Oops, there is no country with that name'));
+};
+input.addEventListener('input', debounce(changeInput, DEBOUNCE_DELAY));
+
+//render
+function renderCountryList(name) {
+  const markup = name
+    .map(name => {
+      return `<li>
+          <p><b>Name</b>: ${name.name}</p>
+        </li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
