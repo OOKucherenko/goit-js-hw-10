3704587dcb845fb831aefd2d70a00a328bdb0e33
OOKucherenko <okld52dec@gmail.com>
2023-02-03 19:17:09 +0200

    task

---
+++ src/fetchCountries.js
@@ -0,0 +1,28 @@
+// https://restcountries.com/v2/all?fields=name,capital,currencies
+
+const fetchCountries = name => {
+  fetch(
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
+
+//render
+function renderUserList(users) {
+  const markup = users
+    .map(user => {
+      return `<li>
+          <p><b>Name</b>: ${user.name}</p>
+          <p><b>Email</b>: ${user.email}</p>
+          <p><b>Company</b>: ${user.company.name}</p>
+        </li>`;
+    })
+    .join('');
+  userList.innerHTML = markup;
+}
+
+export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
-
+import fetchCountries from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+let name = '';
+const input = document.querySelector('input#search-box');
+input.addEventListener('input', e => {
+  name = e.currentTarget.value;
+  fetchCountries(name);
+});
